9ba26e99b73e1cce191f66bea9492a79385a5e3f
Luis Valencia <102180625+lvcodes3@users.noreply.github.com>
2022-12-07 17:27:26 -0800

    Added comments to instructor.py

--- instructor.py
+++ instructor.py
@@ -2,10 +2,21 @@ import socket
 import threading
 import random
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # (internet socket, UDP)
+# AF_INET is the address family (default), in our case internet socket
+# SOCK_DGRAM is the type of socket, in our case UDP
+# client instructor will be assigned a new socket (internet socket, UDP)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# port number for instructor will be 9000
 port = 9000
+
+# asking user to type a username for the client instructor
 username = input("Enter a username: ")
-client.bind(("localhost", 9000))
+
+# when presenting, no need to specify different ports
+# all clients will have a hostname of "localhost", but a different port (used for the AF_INET address family)
+# we bind this information to the client instructor socket
+client.bind(("localhost", port))
 
 def rcv(): #handles recieving messages from server and prints them
     while True:
@@ -16,15 +27,23 @@ def rcv(): #handles recieving messages from server and prints them
         except:
             pass
 
+# define thread and start thread
 thread1 = threading.Thread(target=rcv)
 thread1.start()
 
-client.sendto(f"CLIENT_ADDED:{username}".encode(), ("localhost", 9999)) #specifies new client so server can notify everyone that new client joined
+# specifies new client so server can notify everyone that new client joined
+client.sendto(f"INSTRUCTOR_ADDED: {username}".encode(), ("localhost", 9999))
 
 while True: #send user input to server
+    # asking instructor client for input message to send to server
     msg = input("")
+    # code removed
+    #msg = input(f"{username}: ")
+    #idx = msg.find(":")
+    #msg = msg[idx + 1 : len(msg)]
     if msg == "quit":
         exit()
     else:
+        # sending the message to the server
         client.sendto(f'{username} (Instructor): {msg}'.encode(), ("localhost", 9999))
 
